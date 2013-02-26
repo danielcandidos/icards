ee2e0ea51c17e98a2461599df0dae265093889da
itsgustavopereira <itsgustavopereira@gmail.com>
2013-02-26 20:08:05 +0000

    Teste legal de classes entre pacotes.

---
+++ Cards/src/cards/controller/mainTeste.java
@@ -0,0 +1,14 @@
+package cards.controller;
+import cards.model.*;
+
+/**
+ *
+ * @author gustavopereira
+ */
+public class mainTeste {
+   
+    public static void main(String[] args){
+        TesteRepositorio objeto = new TesteRepositorio();
+        objeto.testeClasse("Gustavo");
+    }
+}
