bbbb6ba15c43dad2bc1e584259721a51fe4a72db
itsgustavopereira <itsgustavopereira@gmail.com>
2013-03-01 04:28:57 +0000

    [pkg negocio] Classe GerenciarUsuario criada

--- Cards/src/negocio/GerenciarUsuario.java
+++ Cards/src/negocio/GerenciarUsuario.java
@@ -1,9 +1,42 @@
 package negocio;
+import bean.Cartao;
+import bean.Usuario;
 
 /**
  *
  * @author gustavopereira
  */
 public class GerenciarUsuario {
+    private Usuario usuario;
+
+    public GerenciarUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    
+    public void alterarSenhaOnline(String senha){
+        //Desenvolver
+    }
+    
+    public void alterarSenhaCartao(int senha){
+        //Desenvolver
+    }
+    
+    public void alterarEmail(String email){
+        //Desenvolver
+    }
+    
+    public void alterarEndereco(String rua){
+        //Desenvolver
+    }
+    
+    public void alterarTipoUsuario(int tipo){
+        //Desenvolver
+    }
+    
+    public void alterarCartao(Cartao cartao){
+        this.usuario.setCartao(cartao);
+        //Desenvolver
+    }
+    
     
 }
