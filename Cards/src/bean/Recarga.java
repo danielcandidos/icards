522b7eb23ebb36ee11d0e21bb20d156573b27ed2
itsgustavopereira <itsgustavopereira@gmail.com>
2013-03-01 03:41:53 +0000

    Classe Recarga finalizada.

---
+++ Cards/src/bean/Recarga.java
@@ -0,0 +1,48 @@
+package bean;
+
+/**
+ *
+ * @author gustavopereira
+ */
+public class Recarga {
+    private float valor;
+    private String pessoa;
+    private String data;
+    private String mensagem;
+
+    public Recarga () {    
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public String getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(String pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+    
+}
