96ddc5f4c139af627dfbb3be305965005b67547d
itsgustavopereira <itsgustavopereira@gmail.com>
2013-03-01 04:19:10 +0000

    [pkg negocio] Classe GerenciarDebito criada

--- Cards/src/negocio/GerenciarDebito.java
+++ Cards/src/negocio/GerenciarDebito.java
@@ -1,9 +1,22 @@
 package negocio;
+import bean.Debito;
 
 /**
  *
  * @author gustavopereira
  */
 public class GerenciarDebito {
+    private Debito debito;
+
+    public GerenciarDebito(Debito debito) {
+        this.debito = debito;
+    }
     
+    public void debitar(float valor){
+        //Desenvolver
+    }
+    
+    public void salvarExtrato(){
+        //Desenvolver parte do banco
+    } 
 }
