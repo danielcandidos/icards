54548b5317da8c4a30482924096fdeebaa3355a3
itsgustavopereira@gmail.com <itsgustavopereira@gmail.com@fbcde275-624e-8db7-8827-5833a2ec6f27>
2013-03-01 04:23:47 +0000

    [pkg negocio] Classe GerenciarRecarga criada

--- trunk/Cards/src/negocio/GerenciarRecarga.java
+++ trunk/Cards/src/negocio/GerenciarRecarga.java
@@ -1,9 +1,23 @@
 package negocio;
+import bean.Recarga;
 
 /**
  *
  * @author gustavopereira
  */
 public class GerenciarRecarga {
+    private Recarga recarga;
+
+    public GerenciarRecarga(Recarga recarga) {
+        this.recarga = recarga;
+    }
+    
+    public void solicitarRecarga(){
+        //Desenvolver
+    }
+    
+    public void salvarRecarga(){
+        //Desenvolver parte de banco
+    }
     
 }
