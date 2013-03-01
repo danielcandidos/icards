384a56d362bfa8a805e59667703ee4136db75cf3
itsgustavopereira <itsgustavopereira@gmail.com>
2013-03-01 04:22:02 +0000

    [pkg negocio] Classe GerenciarEstabelecimento criada

--- Cards/src/negocio/GerenciarEstabelecimento.java
+++ Cards/src/negocio/GerenciarEstabelecimento.java
@@ -1,9 +1,31 @@
 package negocio;
+import bean.Estabelecimento;
 
 /**
  *
  * @author gustavopereira
  */
 public class GerenciarEstabelecimento {
+    private Estabelecimento estabelecimento;
+
+    public GerenciarEstabelecimento(Estabelecimento estabelecimento) {
+        this.estabelecimento = estabelecimento;
+    }
+    
+    public void alterarNome(String nome){
+        //Desenvolver
+    }
+    
+    public void alterarEndereco(String rua){
+        //Desenvolver
+    }
+    
+    public void alterarTelefone(String fone){
+        //Desenvolver
+    }
+    
+    public void alterarCNPJ(int CNPJ){
+        //Desenvolver
+    }
     
 }
