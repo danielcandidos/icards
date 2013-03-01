d09da124f38386a98177c58bf6be544c465d8fa3
itsgustavopereira@gmail.com <itsgustavopereira@gmail.com@fbcde275-624e-8db7-8827-5833a2ec6f27>
2013-03-01 03:46:01 +0000

    Classe Estabelecimento finalizada.

---
+++ trunk/Cards/src/bean/Estabelecimento.java
@@ -0,0 +1,48 @@
+package bean;
+
+/**
+ *
+ * @author gustavopereira
+ */
+public class Estabelecimento {
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private int CNPJ;
+    
+    public Estabelecimento() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getCNPJ() {
+        return CNPJ;
+    }
+
+    public void setCNPJ(int CNPJ) {
+        this.CNPJ = CNPJ;
+    }
+    
+}
