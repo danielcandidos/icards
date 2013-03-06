66bdc7c9915b604a3afd5e24f619699f5880910b
itsgustavopereira <itsgustavopereira@gmail.com>
2013-03-06 22:09:54 +0000

    Criação da conexão com o banco de dados na classe GerenciarDB dando erro. Descobrir o ERRO.

--- Cards/src/negocio/GerenciarDB.java
+++ Cards/src/negocio/GerenciarDB.java
@@ -1,8 +1,47 @@
 package negocio;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 /**
  *
  * @author gustavopereira
  */
 public class GerenciarDB {
-    //coming soon
+    static Connection conexao;
+   
+    public static void conectaDB(){
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:icards.serveftp.com";
+        String login = "panda";
+        String password = "bazinga";
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url,login,password);
+            System.out.println("Conexao bem sucedida");
+        } catch (Exception e){
+            System.out.println("Conexao falhou");
+        }
+    }
+    
+    public static void desconectaDB()throws Exception{
+        conexao.close();
+    }
+    
+    public static void executaDB(String query)throws Exception{
+        Statement exe = conexao.createStatement();
+        exe.executeUpdate(query);
+    }
+    
+    public void addUsuario(int cpf,String nome,String email,String endereco,String telefone,String datanascimento,String nacionalidade,int tipousuario,int cpftitular,String senhaadm) throws Exception{
+        conectaDB();
+        System.out.print("OK ate aqui");
+        String query;
+        query = "insert into USUARIO (cpf,nome,email,endereco,telefone,datanascimento,nacionalidade,tipousuario,cpftitular,senhaadm) "
+                + "values (cpf,nome,email,endereco,telefone,datanascimento,nacionalidade,tipousuario,cpftitular,senhaadm)";
+        executaDB(query);
+        desconectaDB();
+    }
 }
--- Cards/src/xCards/mainTeste.java
+++ Cards/src/xCards/mainTeste.java
@@ -1,13 +1,15 @@
 package xCards;
 
+import negocio.GerenciarDB;
+
 /**
  *
  * @author gustavopereira
  */
 public class mainTeste {
    
-    public static void main(String[] args){
-        TesteRepositorio objeto = new TesteRepositorio();
-        objeto.testeClasse("Rafaella");
+    public static void main(String[] args) throws Exception{
+        GerenciarDB banco = new GerenciarDB();
+        banco.addUsuario(666666666,"Capeta","hell@bol.com","Inferno Av.","66326694","06/06/06","Hell",1,1234,"adm1234senha");
     }
 }
