a31d8839920397e6145915dd05598e600f1e0f3c
itsgustavopereira@gmail.com <itsgustavopereira@gmail.com@fbcde275-624e-8db7-8827-5833a2ec6f27>
2013-03-10 22:34:56 +0000

    Função de DELETE do banco de dados. Todas as tabelas funcionando.

--- trunk/Cards/src/xCards/mainTeste.java
+++ trunk/Cards/src/xCards/mainTeste.java
@@ -14,6 +14,13 @@ public class mainTeste {
         //banco.addEstabelecimento(12345, "Casa Loterica", "34543433");
         //banco.addCartao(0,"13");
         //banco.addExtratoDeposito(13.0, "Gustavo", 15.50, "10/03/2013");
-        banco.addExtratoDebito(12345.0, 13.0, 345.20, "11/03/2013");
+        //banco.addExtratoDebito(12345.0, 13.0, 345.20, "11/03/2013");
+        //banco.delUsuario("135555");
+        
+        //banco.delExtrato(4);
+        //banco.delExtrato(3);
+        //banco.delEstabelecimento(12345);
+        //banco.delCartao(13);
+      
     }
 }
