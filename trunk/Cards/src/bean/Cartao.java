5d557d10ce2f838f3c88e0ffb7d54a63384ca9c2
itsgustavopereira@gmail.com <itsgustavopereira@gmail.com@fbcde275-624e-8db7-8827-5833a2ec6f27>
2013-03-17 04:08:04 +0000

    Update bean

--- trunk/Cards/src/bean/Cartao.java
+++ trunk/Cards/src/bean/Cartao.java
@@ -7,9 +7,16 @@ package bean;
 public class Cartao {
     private String numero;
     private double saldo;
-    
+    private String senhaOnlineTitular;
+    private String senhaOnlineDependente;
+    private String senhaCartao;
+
     public Cartao(String numero) {
         this.numero = numero;
+    }
+
+    public String getSenhaOnlineTitular() {
+        return senhaOnlineTitular;
     }
 
     public String getNumero() {
@@ -19,7 +26,7 @@ public class Cartao {
     public void setNumero(String numero) {
         this.numero = numero;
     }
-    
+
     public double getSaldo() {
         return saldo;
     }
@@ -27,4 +34,21 @@ public class Cartao {
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
+
+    public String getSenhaOnlineDependente() {
+        return senhaOnlineDependente;
+    }
+
+    public void setSenhaOnlineDependente(String senhaOnlineDependente) {
+        this.senhaOnlineDependente = senhaOnlineDependente;
+    }
+
+    public String getSenhaCartao() {
+        return senhaCartao;
+    }
+
+    public void setSenhaCartao(String senhaCartao) {
+        this.senhaCartao = senhaCartao;
+    }
+
 }
