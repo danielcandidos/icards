27ad6c30d1e096ab7a9282f1a45cde234fb5a3db
hanna.pereira13 <hanna.pereira13@gmail.com>
2013-03-18 21:58:11 +0000

    mudança no nome

--- Cards/src/visao/JanelaEstabelecimento.java
+++ Cards/src/visao/JanelaEstabelecimento.java
@@ -11,12 +11,12 @@ import javax.swing.JOptionPane;
  *
  * @author Uguinho
  */
-public class JanelaInicioEstabelecimento extends javax.swing.JFrame {
+public class JanelaEstabelecimento extends javax.swing.JFrame {
 
     /**
-     * Creates new form JanelaInicioEstabelecimento
+     * Creates new form JanelaEstabelecimento
      */
-    public JanelaInicioEstabelecimento() {
+    public JanelaEstabelecimento() {
         initComponents();
     }
 
@@ -125,20 +125,20 @@ public class JanelaInicioEstabelecimento extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(JanelaInicioEstabelecimento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JanelaEstabelecimento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(JanelaInicioEstabelecimento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JanelaEstabelecimento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(JanelaInicioEstabelecimento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JanelaEstabelecimento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(JanelaInicioEstabelecimento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JanelaEstabelecimento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new JanelaInicioEstabelecimento().setVisible(true);
+                new JanelaEstabelecimento().setVisible(true);
             }
         });
     }
