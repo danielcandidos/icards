b5af37d1a3ea1fff8fdcd0e70afd220ba55b865b
hanna.pereira13@gmail.com <hanna.pereira13@gmail.com@fbcde275-624e-8db7-8827-5833a2ec6f27>
2013-03-20 04:10:23 +0000

    deletarEst

--- trunk/Cards/src/visao/JanelaDeletarUsuario.java
+++ trunk/Cards/src/visao/JanelaDeletarUsuario.java
@@ -4,6 +4,10 @@
  */
 package visao;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import negocio.GerenciarDB;
+
 /**
  *
  * @author Rafaella
@@ -31,6 +35,7 @@ public class JanelaDeletarUsuario extends javax.swing.JFrame {
         DelUsuario = new javax.swing.JFormattedTextField();
         ConfirmarDelUsuario = new javax.swing.JButton();
         CancelarDelUsuario = new javax.swing.JButton();
+        todosCampos = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Deletar Usuário");
@@ -47,6 +52,11 @@ public class JanelaDeletarUsuario extends javax.swing.JFrame {
         }  
         catch (Exception e){  
         }
+        DelUsuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                DelUsuarioActionPerformed(evt);
+            }
+        });
 
         ConfirmarDelUsuario.setText("Confirmar");
         ConfirmarDelUsuario.addActionListener(new java.awt.event.ActionListener() {
@@ -67,22 +77,23 @@ public class JanelaDeletarUsuario extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(33, 33, 33)
-                        .addComponent(DeletarCPF)
-                        .addGap(18, 18, 18)
-                        .addComponent(DelUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(213, 213, 213)
-                        .addComponent(DeletarUsuario)))
+                .addGap(213, 213, 213)
+                .addComponent(DeletarUsuario)
                 .addGap(219, 234, Short.MAX_VALUE))
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(183, 183, 183)
                 .addComponent(ConfirmarDelUsuario)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(CancelarDelUsuario)
                 .addGap(210, 210, 210))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(33, 33, 33)
+                .addComponent(DeletarCPF)
+                .addGap(18, 18, 18)
+                .addComponent(DelUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(todosCampos, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(204, 204, 204))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -92,7 +103,8 @@ public class JanelaDeletarUsuario extends javax.swing.JFrame {
                 .addGap(69, 69, 69)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(DeletarCPF)
-                    .addComponent(DelUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(DelUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(todosCampos, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 221, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(ConfirmarDelUsuario)
@@ -113,9 +125,34 @@ public class JanelaDeletarUsuario extends javax.swing.JFrame {
 
     private void ConfirmarDelUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ConfirmarDelUsuarioActionPerformed
         // TODO add your handling code here:
-        String deletarCPF = DelUsuario.getText().replaceAll("[.]","");
-        deletarCPF = deletarCPF.replaceAll("[-]", "");
+        String CPF = DelUsuario.getText().replaceAll("[.]","");
+        CPF = CPF.replaceAll("[-]", "");
+        
+        if ((CPF.isEmpty()) || ("        ".equals(CPF)) ) {
+            todosCampos.setText("O campo deve ser preenchido");
+        } else {
+            try {
+                 GerenciarDB banco = new GerenciarDB();
+                 
+                 if (banco.checkUsuarioDB(CPF)) {
+                    banco.delUsuario(CPF);
+                    JanelaInicioAdmin frame = new JanelaInicioAdmin();
+                    frame.setLocationRelativeTo(null);
+                    frame.setVisible(true);
+                    this.dispose();
+                 } else {
+                     System.out.println("NAO EXISTE ESSE CARTAO ERRO");
+                 }
+            } catch (Exception ex) {
+                Logger.getLogger(JanelaDeletarCartao.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
     }//GEN-LAST:event_ConfirmarDelUsuarioActionPerformed
+
+    private void DelUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DelUsuarioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_DelUsuarioActionPerformed
 
     /**
      * @param args the command line arguments
@@ -157,5 +194,6 @@ public class JanelaDeletarUsuario extends javax.swing.JFrame {
     private javax.swing.JFormattedTextField DelUsuario;
     private javax.swing.JLabel DeletarCPF;
     private javax.swing.JLabel DeletarUsuario;
+    private javax.swing.JLabel todosCampos;
     // End of variables declaration//GEN-END:variables
 }
