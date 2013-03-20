731b35381917c0787fb31fb6f8bec1e8cba480fb
rafaella-pe@hotmail.com <rafaella-pe@hotmail.com@fbcde275-624e-8db7-8827-5833a2ec6f27>
2013-03-20 01:31:39 +0000

    Renomeada JanelaDebito

--- trunk/Cards/src/visao/JanelaDebito.java
+++ trunk/Cards/src/visao/JanelaDebito.java
@@ -11,12 +11,12 @@ import javax.swing.JOptionPane;
  *
  * @author Uguinho
  */
-public class JanelaEstabelecimento extends javax.swing.JFrame {
+public class JanelaDebito extends javax.swing.JFrame {
 
     /**
      * Creates new form JanelaEstabelecimento
      */
-    public JanelaEstabelecimento() {
+    public JanelaDebito() {
         initComponents();
     }
 
@@ -159,20 +159,20 @@ public class JanelaEstabelecimento extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(JanelaEstabelecimento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JanelaDebito.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(JanelaEstabelecimento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JanelaDebito.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(JanelaEstabelecimento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JanelaDebito.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(JanelaEstabelecimento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JanelaDebito.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new JanelaEstabelecimento().setVisible(true);
+                new JanelaDebito().setVisible(true);
             }
         });
     }
