5f4bf7fdf27913745e9f8a16dc5dc7adf5ad13ca
itsgustavopereira@gmail.com <itsgustavopereira@gmail.com@fbcde275-624e-8db7-8827-5833a2ec6f27>
2013-03-21 15:44:12 +0000

    Ainda dando erro no Usuario. É o table q mudou.

--- trunk/Cards/src/negocio/GerarRelatorioPDF.java
+++ trunk/Cards/src/negocio/GerarRelatorioPDF.java
@@ -5,10 +5,8 @@ import com.itextpdf.text.Document;
 import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.Font.FontFamily;
-import com.itextpdf.text.Image;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
